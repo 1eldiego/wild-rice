44c966865345a1ad13dae1ec65b0b5998a162595
diegoxido <balladares.diego@gmail.com>
2017-02-11 23:59:52 -0300

    agrego babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: __dirname + '/dist/',
-    filename: 'poc.js',
+    path: path.join(__dirname, '/dist/'),
+    filename: 'wild-rice.js',
   },
   module: {
     loaders: [
       {
         test: /\.less$/,
         loader: 'style!css!less',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
       },
     ],
   },
